754a2f11cfde22b6a36a69e72e1855ba84864f48
maxacan <91285870+maxacan@users.noreply.github.com>
2021-12-03 15:23:24 -0300

    Configuración de templates básicos con archivos HTML temporales, junto con una simple vista de prueba en app/post/views.py

--- BlogInfoBase/urls.py
+++ BlogInfoBase/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.post.views import inicio_view
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path("", inicio_view)
 ]
--- app/post/views.py
+++ app/post/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Post
 
 # Create your views here.
+
+def inicio_view(request):
+    posts = Post.objects.all()
+    return render(request, 'base/index.html', context={"posts":posts})
