fc3bbd19c18160be70a9e815c6c3344ec91004c9
Ferclemens <69605971+Ferclemens@users.noreply.github.com>
2021-12-19 20:11:51 -0300

    nuevo form usuarios (+ nombre, apellido, mail)

--- app/usuarios/views.py
+++ app/usuarios/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
-
+from django.urls import reverse_lazy
+from django.views.generic import CreateView
+from django.contrib.auth.models import User
+from .forms import RegistroUsuarioFrom
 # Create your views here.
+
+class CrearUsuarioForm(CreateView):
+	model = User
+	form_class = RegistroUsuarioFrom
+	template_name = 'registration/register.html'
+	success_url = reverse_lazy('inicio')
